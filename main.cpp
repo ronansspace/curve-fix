f7438f132c275c114bd3c0da1dae8ec2924104aa
ronieh <ronanhughes@Ronans-MacBook-Pro.local>
2017-01-24 21:45:38 +0000

    Cmake build using installed quickfix

--- main.cpp
+++ main.cpp
@@ -1,6 +1,35 @@
-#include <iostream>
+#include "quickfix/FileStore.h"
+#include "quickfix/SocketInitiator.h"
+#include "Application.h"
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+int main( int argc, char** argv )
+{
+    if ( argc != 2 )
+    {
+        std::cout << "usage: " << argv[ 0 ]
+                  << " FILE." << std::endl;
+        return 0;
+    }
+    std::string file = argv[ 1 ];
+
+    try
+    {
+        FIX::SessionSettings settings( file );
+
+        Application application;
+        FIX::FileStoreFactory storeFactory( settings );
+        FIX::ScreenLogFactory logFactory( settings );
+        FIX::SocketInitiator initiator( application, storeFactory, settings, logFactory );
+
+        initiator.start();
+        application.run();
+        initiator.stop();
+
+        return 0;
+    }
+    catch ( std::exception & e )
+    {
+        std::cout << e.what();
+        return 1;
+    }
 }
