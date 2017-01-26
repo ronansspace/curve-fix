c82090f28e386f8d22e0f8f1be6801b9af00684b
ronieh <ronanhughes@Ronans-MacBook-Pro.local>
2017-01-26 20:19:28 +0000

    Checking Writing to file

--- src/Application/Application.cpp
+++ src/Application/Application.cpp
@@ -1,4 +1,5 @@
 #include "Application/Application.h"
+#include <fstream>
 
 void Application::onLogon( const FIX::SessionID& sessionID )
 {
@@ -13,9 +14,7 @@ void Application::onLogout( const FIX::SessionID& sessionID )
 void Application::fromApp( const FIX::Message& message, const FIX::SessionID& sessionID )
 throw( FIX::FieldNotFound, FIX::IncorrectDataFormat, FIX::IncorrectTagValue, FIX::UnsupportedMessageType )
 {
-    std::cout << std::endl << "OUTOUT: " << message << std::endl;
-    crack( message, sessionID );
-    std::cout << std::endl << "IN: " << message << std::endl;
+    crack( message, sessionID ); // Takes the message and routes to the appropriate onMessage function.
 }
 
 void Application::toApp( FIX::Message& message, const FIX::SessionID& sessionID )
@@ -26,6 +25,12 @@ throw( FIX::DoNotSend )
 void Application::onMessage
         ( const FIX44::ExecutionReport& executionReport, const FIX::SessionID& sessionID) {
     std::cout << "Got an Execution Report" << std::endl;
+    std::fstream fs;
+    fs.open ("test.txt", std::fstream::in | std::fstream::out | std::fstream::app);
+    fs << executionReport.toString();
+    fs.close();
+
+
 }
 
 void Application::run()
