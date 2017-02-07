812ee60dcaa08dbaf96b9443852f33198d246f28
ronieh <ronanhughes@Ronans-MacBook-Pro.local>
2017-02-07 14:04:59 +0000

    new MD engine

--- src/Application/Application.cpp
+++ src/Application/Application.cpp
@@ -5,6 +5,33 @@ using namespace std;
 void Application::onLogon( const FIX::SessionID& sessionID )
 {
     cout << endl << "We are logged on - " << sessionID << endl;
+
+    FIX::MDReqID mdReqID( "MARKETDATAID" );
+    FIX::SubscriptionRequestType subType( FIX::SubscriptionRequestType_SNAPSHOT );
+    FIX::MarketDepth marketDepth( 0 );
+
+    FIX44::MarketDataRequest::NoMDEntryTypes marketDataEntryGroup;
+    FIX::MDEntryType mdEntryType( FIX::MDEntryType_BID );
+    marketDataEntryGroup.set( mdEntryType );
+
+    FIX44::MarketDataRequest::NoRelatedSym symbolGroup;
+    FIX::Symbol symbol( "EUR/USD" );
+    symbolGroup.set( symbol );
+
+    FIX44::MarketDataRequest message( mdReqID, subType, marketDepth );
+    message.addGroup( marketDataEntryGroup );
+    message.addGroup( symbolGroup );
+
+    FIX44::Header header = message.getHeader();
+
+    header.setField(FIX::SenderCompID("FIXUAT16-MD"));
+    header.setField(FIX::TargetCompID("TTUAT-MD"));
+
+    std::cout << message.toXML() << std::endl;
+    std::cout << message.toString() << std::endl;
+
+    FIX::Session::sendToTarget(message, sessionID);
+
 }
 
 void Application::onLogout( const FIX::SessionID& sessionID )
@@ -27,32 +54,6 @@ void Application::onMessage
         ( const FIX44::ExecutionReport& executionReport, const FIX::SessionID& sessionID) {
 
     cout << "Processing Execution Report - " << sessionID << endl;
-
-    FIX::MDReqID mdReqID( "MARKETDATAID" );
-    FIX::SubscriptionRequestType subType( FIX::SubscriptionRequestType_SNAPSHOT );
-    FIX::MarketDepth marketDepth( 0 );
-
-    FIX44::MarketDataRequest::NoMDEntryTypes marketDataEntryGroup;
-    FIX::MDEntryType mdEntryType( FIX::MDEntryType_BID );
-    marketDataEntryGroup.set( mdEntryType );
-
-    FIX44::MarketDataRequest::NoRelatedSym symbolGroup;
-    FIX::Symbol symbol( "EUR/USD" );
-    symbolGroup.set( symbol );
-
-    FIX44::MarketDataRequest message( mdReqID, subType, marketDepth );
-    message.addGroup( marketDataEntryGroup );
-    message.addGroup( symbolGroup );
-
-    FIX44::Header header = message.getHeader();
-
-    header.setField(FIX::SenderCompID("FIXUAT16-ORD"));
-    header.setField(FIX::TargetCompID("TTUAT-ORD"));
-
-    std::cout << message.toXML() << std::endl;
-    std::cout << message.toString() << std::endl;
-
-    FIX::Session::sendToTarget(message);
 
     unique_ptr<ExecutionReportHandler> execReport(new ExecutionReportHandler());
     execReport->toFile(executionReport, "test.out");
