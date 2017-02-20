a7e042e6fb163bd17beb991289bad1e1b0ba2462
ronieh <ronanhughes@Ronans-MacBook-Pro.local>
2017-02-20 20:32:24 +0000

    live market data

--- src/MarketDataReport/MarketDataRequestor.cpp
+++ src/MarketDataReport/MarketDataRequestor.cpp
@@ -5,36 +5,44 @@ using namespace std;
 
 void MarketDataRequestor::send(const FIX::SessionID& sessionID) const {
 
-    sql::Driver *driver = get_driver_instance();
+    try {
+        sql::Driver *driver = get_driver_instance();
 
-    unique_ptr<sql::Connection> con(driver->connect("tcp://localhost", "root", "bc43f15f516460e8966700a05761371e0235799a6d86ffd7"));
-    con->setSchema("cmarkets");
+        unique_ptr<sql::Connection> con(driver->connect("tcp://localhost", "root", "f6a23e727b2ec091c4a42d117ef744bcf8ee7dbc59802700"));
+        con->setSchema("cmarkets");
 
-    unique_ptr<sql::PreparedStatement> pstmt(con->prepareStatement("SELECT ccypair from ccyrate"));
-    unique_ptr<sql::ResultSet> res(pstmt->executeQuery());
+        unique_ptr<sql::PreparedStatement> pstmt(con->prepareStatement("SELECT ccypair from ccyrate"));
+        unique_ptr<sql::ResultSet> res(pstmt->executeQuery());
 
-    res->afterLast();
+        res->afterLast();
 
-    while (res->previous()) {
-        string iCcyPair = res->getString("ccypair");
-        iCcyPair.insert(3, "/");
-        cout << "Requesting " + iCcyPair << endl;
+        while (res->previous()) {
+            string iCcyPair = res->getString("ccypair");
+            iCcyPair.insert(3, "/");
+            cout << "Requesting " + iCcyPair << endl;
 
-        string requestID = "Request" + iCcyPair;
-        FIX::MDReqID mdReqID( requestID );
-        FIX::SubscriptionRequestType subType( FIX::SubscriptionRequestType_SNAPSHOT_PLUS_UPDATES );
-        FIX::MarketDepth marketDepth( 0 );
-        FIX44::MarketDataRequest::NoMDEntryTypes marketDataEntryGroup;
-        FIX::MDEntryType mdEntryType( FIX::MDEntryType_BID );
-        marketDataEntryGroup.set( mdEntryType );
-        FIX44::MarketDataRequest::NoRelatedSym symbolGroup;
-        symbolGroup.set( FIX::Symbol(iCcyPair) );
-        FIX44::MarketDataRequest message( mdReqID, subType, marketDepth);
-        message.setField(FIX::MDUpdateType(0));
-        message.addGroup( marketDataEntryGroup );
-        message.addGroup( symbolGroup );
-        FIX::Session::sendToTarget(message, sessionID);
-    }
-
+            string requestID = "Request" + iCcyPair;
+            FIX::MDReqID mdReqID( requestID );
+            FIX::SubscriptionRequestType subType( FIX::SubscriptionRequestType_SNAPSHOT_PLUS_UPDATES );
+            FIX::MarketDepth marketDepth( 0 );
+            FIX44::MarketDataRequest::NoMDEntryTypes marketDataEntryGroup;
+            FIX::MDEntryType mdEntryType( FIX::MDEntryType_BID );
+            marketDataEntryGroup.set( mdEntryType );
+            FIX44::MarketDataRequest::NoRelatedSym symbolGroup;
+            symbolGroup.set( FIX::Symbol(iCcyPair) );
+            FIX44::MarketDataRequest message( mdReqID, subType, marketDepth);
+            message.setField(FIX::MDUpdateType(0));
+            message.addGroup( marketDataEntryGroup );
+            message.addGroup( symbolGroup );
+            FIX::Session::sendToTarget(message, sessionID);
+        }
+        } catch (sql::SQLException &e) {
+            cout << "# ERR: SQLException in " << __FILE__;
+            cout << "# ERR: " << e.what();
+            cout << " (MySQL error code: " << e.getErrorCode();
+            cout << ", SQLState: " << e.getSQLState() << " )" << endl;
+        } catch (exception& e) {
+            cout << e.what() << endl;
+        }
 }
 
