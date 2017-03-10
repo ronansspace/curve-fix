5dba6cf6a20987b4883655cfe0eb85838c50c34f
ronieh <ronanhughes@Ronans-MacBook-Pro.local>
2017-03-10 01:14:26 +0000

    install

--- build.sh
+++ build.sh
@@ -4,3 +4,5 @@ sudo cmake .. -DCMAKE_INSTALL_PREFIX=/usr/bin
 sudo make clean
 sudo make
 sudo make install
+sudo chmod 777 /usr/bin/curve_fix/run-TT-MD-live.sh
+sudo chmod 777 /usr/bin/curve_fix/run-TT-live.sh
