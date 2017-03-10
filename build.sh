cb6d842bfdb2aecd30f41090f4977d1c37f021e0
ronieh <ronanhughes@Ronans-MacBook-Pro.local>
2017-03-10 00:55:03 +0000

    install

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
-mkdir build
+sudo mkdir build
 cd build
-cmake ..
-make
+sudo cmake .. -DCMAKE_INSTALL_PREFIX=/usr/bin
+sudo make clean
+sudo make
+sudo make install
---
+++ run-TT-MD-live.sh
@@ -0,0 +1,2 @@
+export LD_LIBRARY_PATH=/usr/local/share/mysql/mysql-connector-c++-1.1.8/lib
+/usr/bin/curve_fix/curve_fix /usr/bin/curve_fix/cfg/trader-tools-MD-engine-live.cfg true TraderTools
