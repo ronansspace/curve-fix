02f72541677a539668bfea6aa2b70047d8c9c86d
ronieh <ronanhughes@Ronans-MacBook-Pro.local>
2017-03-14 09:37:34 +0000

    Flex Trade live config

--- run-FlexTrade-live.sh
+++ run-FlexTrade-live.sh
@@ -1,2 +1,2 @@
 export LD_LIBRARY_PATH=/usr/local/share/mysql/mysql-connector-c++-1.1.8/lib
-/usr/bin/curve_fix/curve_fix /usr/bin/curve_fix/cfg/lex-trade-engine-live.cfg false FlexTrade
+/usr/bin/curve_fix/curve_fix /usr/bin/curve_fix/cfg/flex-trade-engine-live.cfg false FlexTrade
